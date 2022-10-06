9ca390ebe427640f29290cfe91bac92fa4775029
Noureddine Legragui <noureddinelegragui@gmail.com>
2022-10-06 14:26:27 +0100

    [Commit-1] Initial Xcode project

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Noureddine Legragui on 6/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
